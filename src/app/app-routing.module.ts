19e43db4f81a9b1e1a5447f0978f6592abd0466a
carol975 <carol975@users.noreply.github.com>
2020-07-08 21:18:46 -0700

    add post editor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
+  {path:'new', component: CreatePostComponent},
   {path: '**', redirectTo: 'home'}
 ];
 
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import MediumEditor from 'medium-editor';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent implements OnInit {
+export class CreatePostComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('editable') editable: ElementRef;
+
+  editor;
   constructor() { }
+  ngAfterViewInit(): void {
+    const edit = this.editable.nativeElement;
+    this.editor = new MediumEditor(edit);
+    console.log(this.editor.getContent());
+  }
 
   ngOnInit(): void {
   }
